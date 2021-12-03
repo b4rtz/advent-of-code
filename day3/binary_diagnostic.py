ca92fd791ebfa76b71723c8a0b0364e5cf3e2284
b4rtz <brian.artz@silobreaker.com>
2021-12-04 00:06:18 +0100

    Fix typo + other cleanup of day3

--- day3/binary_diagnostic.py
+++ day3/binary_diagnostic.py
@@ -1,6 +1,4 @@
 import sys
-from types import coroutine
-from typing import BinaryIO
 
 def check_power_consumption(filename, part):
     gamma_bits = [] #most common bit of each index of every line from the diagnostic report
@@ -34,13 +32,10 @@ def check_power_consumption(filename, part):
             co2_rate = 1
             for i in range(len(diagnostics)):
                 bits_list = [bit for bit in diagnostics[i].rstrip()]
-                # print(f'bits list len = {len(bits_list)}')
                 for j in range(len(bits_list)+1):
                     bits_dict[i] = bits_list[:j]
 
             oxy_filter_list = bits_dict.values()
-            # print(oxy_filter_list)
-            #oxy_filter_list[0] = ['0', '1', '0', '1', '1', '1', '1', '1', '1', '0', '1', '1']
             for i in range(num_columns):
                 # values = oxy_filter_list
                 column_bits = [bit[i] for bit in oxy_filter_list]
@@ -48,7 +43,6 @@ def check_power_consumption(filename, part):
                 bit_to_filter = most_common
 
                 if (bit_to_filter == ''):
-                    print("equal!")
                     bit_to_filter = 1
                 else:
                     bit_to_filter = int(most_common)
@@ -68,13 +62,14 @@ def check_power_consumption(filename, part):
                 # values = co2_filter_list
                 column_bits = [bit[i] for bit in co2_filter_list]
                 (most_common, least_common) = divide_bits(column_bits)
-                bit_to_filter == least_common
+                bit_to_filter = least_common
 
                 if (bit_to_filter == ''):
-                    print("equal!")
                     bit_to_filter = 0
+                else:
+                    bit_to_filter = int(least_common)
 
-                co2_filter_list = [''.join(item) for item in co2_filter_list if int(item[i]) == int(bit_to_filter)]
+                co2_filter_list = [''.join(item) for item in co2_filter_list if int(item[i]) == bit_to_filter]
 
                 print(f'len of list = {len(co2_filter_list)}')
 
@@ -92,8 +87,6 @@ def keep_func(index, bit_to_filter, row):
         return False
 
 def divide_bits(bit_column: list[str]):
-
-    # print(bit_column)
     ones = bit_column.count('1')
     zeros = bit_column.count('0')
 
@@ -105,11 +98,9 @@ def divide_bits(bit_column: list[str]):
         return ('','')
 
 def convert_binary_to_decimal(bin_list: list[str]):
-    #print("probs like 10")
     binary_str = ''.join(bin_list)
     print(binary_str)
     dec = int(binary_str, 2)
-    # print(dec)
     return dec
 
 if __name__ == '__main__':
